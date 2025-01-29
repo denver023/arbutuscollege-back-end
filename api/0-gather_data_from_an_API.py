0a80111e1102ff4c4ff4a4b0c68dfcd1165edaac
denver023 <denvercayetano23@gmail.com>
2025-01-28 20:03:38 -0800

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -25,7 +25,6 @@ def get_employee_todo_progress(employee_id):
         completed = [task for task in todos if task.get('completed')]
         print("Employee {} is done with tasks({}/{}):".format(
             user.get('name'), len(completed), len(todos)))
-        
         for task in completed:
             print("\t {}".format(task.get('title')))
     except requests.RequestException:
